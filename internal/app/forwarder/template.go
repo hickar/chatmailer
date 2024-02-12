f5aa43bff627bd85009d86b06f885637c7c7a518
Hickar <hickar@icloud.com>
2024-02-12 19:58:31 +0300

    feat: added html-to-markdown converter

--- internal/app/forwarder/template.go
+++ internal/app/forwarder/template.go
@@ -7,6 +7,7 @@ import (
 	"text/template"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/PuerkitoBio/goquery"
 
 	"github.com/hickar/tg-remailer/internal/app/mailer"
 )
@@ -28,14 +29,22 @@ const defaultMessageTemplateString = `
 **To**: {{template "addresses" .To}}
 **Reply To**: {{template "addresses" .ReplyTo}}
 **CC**: {{template "addresses" .CC}}
+**BCC**: {{template "addresses" .BCC}}
 {{with .Subject}}**Subject**: {{.}}{{end}}
 {{with .Date}}**Date**: {{ .Format "Jan 02 2006 15:04:05" }}{{end}}
+{{- range $part := .BodyParts  -}}
+	{{- if eq $part.MIMETYPE "text/html" -}}
+		{{- htmltomarkdown $part.Content -}}
+	{{- else if eq $part.MIMETYPE "text/plain" -}}
+		{{- $part.Content -}}
+	{{- end -}}
+{{- end -}}
 `
 
 var (
 	templateFuncMap = template.FuncMap{
-		"join":     strings.Join,
-		"markdown": convertToMarkDown,
+		"join":           strings.Join,
+		"htmltomarkdown": htmlToMarkDown,
 	}
 	defaultTemplate = template.Must(template.New("").Funcs(templateFuncMap).Parse(defaultMessageTemplateString))
 )
@@ -59,7 +68,29 @@ func renderTemplate(message *mailer.Message, templateStr string) (string, error)
 	return buf.String(), err
 }
 
-func convertToMarkDown(html string) (string, error) {
-	c := md.NewConverter("", true, nil)
+func htmlToMarkDown(html string) (string, error) {
+	c := md.NewConverter("", true, &md.Options{
+		// Fence:              "",
+		// EmDelimiter:        "",
+		StrongDelimiter: "*",
+	}).AddRules(extendedMDRules...)
 	return c.ConvertString(html)
+}
+
+var extendedMDRules = []md.Rule{
+	// Rule for underlined text conversion
+	{
+		Filter: []string{"u", "ins"},
+		Replacement: func(content string, _ *goquery.Selection, _ *md.Options) *string {
+			return md.String("__" + content + "__")
+		},
+	},
+	// Rule for striked through text conversion
+	{
+		Filter: []string{"s", "strike", "del"},
+		Replacement: func(content string, _ *goquery.Selection, _ *md.Options) *string {
+			content = strings.ReplaceAll(content, "~", "\\~")
+			return md.String("~" + content + "~")
+		},
+	},
 }
